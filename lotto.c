5119550852da377c9ccfb87e31ca2315013d1614
Hoyeong Yoo <crystal23733@gmail.com>
2024-11-12 01:42:10 +0900

    로또 뽑기 로직

---
+++ lotto.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void input_nums(int *lotto_nums);
+void print_nums(int *lotto_nums);
+
+int main(void) {
+  int lotto_nums[6];
+
+  input_nums(lotto_nums);
+  print_nums(lotto_nums);
+  return 0;
+}
+
+void input_nums(int *lotto_nums) {}
+
+void print_nums(int *lotto_nums) {}
